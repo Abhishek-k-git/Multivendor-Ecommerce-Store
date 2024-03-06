2cfcdf4b2e573b0f648ad564e83b71232b87c25f
Abhishek kumar <abhishekpatel66029@gmail.com>
2024-03-06 08:40:28 +0530

    ...

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,3 +1,3 @@
 export const server = "https://shopshell-api.vercel.app/api/v1";
 export const client = "https://shopshell.vercel.app";
-export const socket = "https://shopshell-socket.vercel.app";
+export const socket = "https://shopshell-socket.onrender.com";
